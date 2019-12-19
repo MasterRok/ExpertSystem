a1584fccc326a68292b24cbec01c2b978d653ec1
MasterRok <andrey.stankeev@mail.ru>
2019-12-19 23:32:35 +0300

    made Excel cv reader

--- ExpertSystem/CvHandler.cs
+++ ExpertSystem/CvHandler.cs
@@ -1,32 +1,42 @@
-﻿﻿using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using WindowsApplication = System.Windows.Forms.Application;
-using ExcelApplication = Microsoft.Office.Interop.Excel.Application;
+using Excel= Microsoft.Office.Interop.Excel;
 
 namespace ExpertSystem
 {
     public class CvHandler
     {
-        public CvHandler()
-        {
-        }
-
         public Dictionary<string, string> LoadCv(string cvFileName)
         {
-            Console.WriteLine("Cv Handler");
+            var cv = new Dictionary<string, string>();
 
-            ExcelApplication oExcel = new ExcelApplication();
-            Workbook workBook = oExcel.Workbooks.Open(cvFileName);
-            var workSheet = (Worksheet) workBook.Worksheets[0];
-            Console.WriteLine(workSheet.Rows);
+            // Open xlsx file and read from 1st sheet
+            Excel.Application oExcel = new Excel.Application();
+            Workbook workBook = oExcel.Workbooks.Open(cvFileName, null, true);
+            var workSheet = (Worksheet) workBook.Sheets.Item[1];
+            var excelRange = workSheet.UsedRange;
+            
+            // Read cv columns
+            foreach (Range row in excelRange.Rows)
+               cv.Add((string)((Range) excelRange.Cells[row.Row, 1]).Value, 
+                   (string)((Range) excelRange.Cells[row.Row, 2]).Value);
+            
+            // Release objects
+            workBook.Close();
+            oExcel.Quit();
+            Marshal.ReleaseComObject(workSheet);
+            Marshal.ReleaseComObject(workBook);
+            Marshal.ReleaseComObject(oExcel);
 
-            if (true /*If have some uncertainty*/)
+            if (CheckForUncertainty(cv))
             {
-                var cv = new Dictionary<string, string>();
                 var experimenterForm = new Experimenter(cv);
                 if (Control.ModifierKeys == Keys.Control)
                 {
@@ -40,6 +50,13 @@ namespace ExpertSystem
                 else
                     throw new Exception("Error while interview");
             }
+
+            return cv;
+        }
+
+        private bool CheckForUncertainty(Dictionary<string, string> cv)
+        {
+            return false;
         }
     }
 }
