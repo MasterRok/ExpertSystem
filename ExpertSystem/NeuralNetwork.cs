73b2b9709c421edb0d4a3c5b21bf42454f3622c4
MasterRok <andrey.stankeev@mail.ru>
2019-12-27 20:55:30 +0300

    add NN dummy module

---
+++ ExpertSystem/NeuralNetwork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExpertSystem
+{
+    public static class NeuralNetwork
+    {
+        public static bool Analyze(string skillName, string result)
+        {
+            var random = new Random();
+            return random.Next(0, 1) == 0;
+        }
+    }
+}
