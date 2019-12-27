ffb7eb11e44165093778bafc4a25e845b550cbee
MasterRok <andrey.stankeev@mail.ru>
2019-12-27 15:17:05 +0300

    added DataBase dummy

---
+++ ExpertSystem/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpertSystem
+{
+    public static class DataBase
+    {
+        public static void Add(Cv cv)
+        {
+            Console.WriteLine("You have added CV to DB!");
+        }
+    }
+}
